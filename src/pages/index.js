1d4f0250b3cae2d691f5dc080278237d63ff78ed
muhammadzourdy <zhafarmuhammad12@gmail.com>
2021-06-10 23:33:55 +0700

    building featured components: completed --> homePage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Nav from "../components/nav"
+import Featured from "../components/featured"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <Nav />
+    <Featured />
   </Layout>
 )
 
